302f41a5c5ac91fba5177d6b8f966a054567805d
Rick Dennison <rdennison12@gmail.com>
2020-11-02 11:31:42 -0500

    Created OnClientConnect and OnServerAddPlayer

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 
 public class MyNetworkManager : NetworkManager
 {
-   
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("I connected to a server!");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log($"There are now {numPlayers} players");
+    }
 }
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * Initial commit - Repo created
 * Project created and added to version control
 * Begin creating a Network Manager
+* Created OnClientConnect and OnServerAddPlayer
